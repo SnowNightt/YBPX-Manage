36027ee9390fc892755ab2e5864b986f94592c99
SnowNightt <1006817009@qq.com>
2024-06-05 21:05:41 +0800

    feat: 用户首页布局

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,5 +1,20 @@
 <template>
-  <router-view></router-view>
+  <div class="admin w-screen h-screen flex">
+    <Menu />
+    <div class="content flex-1 bg-[#e9ecef]">
+      <i class="fab fa-accessible-icon"></i>
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Menu from './admin/Menu.vue';
+</script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  // 设置该路由下的子路由都需要登录验证
+  route: { meta: { auth: true } }
+});
+</script>
